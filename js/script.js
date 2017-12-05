617d85f7dd5db455abec913b665435c4427a1790
Nenad Curcic <webfanatic82@gmail.com>
2017-12-05 15:55:52 +0100

    Rating implemented from API to FE.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const body = document.getElementsByTagName('body'),
       wrapper = document.getElementById('wrapper'),
   venuesSearchUrl ='https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d1e0931735&v=20131016&ll=44.792455499999996,20.4915168&radius=1000&client_id=O4N5MBHQWS11LRWBBO15JTZWFC42WKSUKTQYMXJ1ZN1CIPXD&client_secret=X1043GY3LH0W4S54GT0RWL300R2144W5WUVJKQ30GI0O1F03&v=20120609'
 
+// Latitude and Longitude 44.792455499999996, 20.4915168
 
 // HTML5 Geolocation
 
@@ -14,8 +15,11 @@ function getLocation() {
 }
 
 function showPosition(position) {
+  let latitude = position.coords.latitude,
+  longitude = position.coords.longitude
   body.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
+  console.log(latitude)
 }
 
 function showError(error) {
@@ -54,39 +58,37 @@ fetch(venuesSearchUrl)
     let places = data.response.venues
 
     return places.map(function(place) {
-      let card = createNode('div'),
-          photoContainer = createNode('div'),
-          infoContainer = createNode('div'),
-          name = createNode('h1'),
-          address = createNode('p'),
-          rating = createNode('p'),
-          coffeeCold = createNode('p'),
-          span = createNode('span')
+      let cardNode = createNode('div'),
+          photoContainerNode = createNode('div'),
+          infoContainerNode = createNode('div'),
+          nameNode = createNode('h1'),
+          addressNode = createNode('p'),
+          ratingNode = createNode('p'),
+          coffeeColdNode = createNode('p'),
+          spanNode = createNode('span'),
 
           idValue = place.id
           nameValue = place.name,
           addressValue = place.location.address, place.location.city
 
-      card.className = "card"
-      photoContainer.className = 'photo-container'
-      infoContainer.className = 'info-container'
-      address.className = 'info-container__address'
-      name.innerHTML = (nameValue) ? nameValue : '-',
-      address.innerHTML = (addressValue) ? addressValue : '-',
-      rating.innerHTML = 'Rating: ',
-      coffeeCold.innerHTML = 'Will the coffee get cold?'
+      cardNode.className = "card"
+      photoContainerNode.className = 'photo-container'
+      infoContainerNode.className = 'info-container'
+      addressNode.className = 'info-container__address'
 
-      append(infoContainer, name),
-      append(infoContainer, address),
-      append(infoContainer, rating),
-      append(infoContainer, coffeeCold),
-      append(card, photoContainer),
-      append(card, infoContainer),
-      append(wrapper, card)
-      console.log(card)
-      //console.log(img.src)
-      console.log(name)
-      console.log(address)
+      nameNode.innerHTML = (nameValue) ? nameValue : '-',
+      addressNode.innerHTML = (addressValue) ? addressValue : 'Sorry, no address found for this venue.',
+      ratingNode.innerHTML = 'Rating: '
+      coffeeColdNode.innerHTML = 'Will the coffee get cold?'
+
+      append(infoContainerNode, nameNode),
+      append(infoContainerNode, addressNode),
+      append(infoContainerNode, ratingNode)
+      append(infoContainerNode, coffeeColdNode),
+      append(cardNode, photoContainerNode),
+      append(cardNode, infoContainerNode),
+      append(ratingNode, spanNode),
+      append(wrapper, cardNode)
 
       fetch('https://api.foursquare.com/v2/venues/' + idValue + '/photos?limit=1&client_id=O4N5MBHQWS11LRWBBO15JTZWFC42WKSUKTQYMXJ1ZN1CIPXD&client_secret=X1043GY3LH0W4S54GT0RWL300R2144W5WUVJKQ30GI0O1F03&v=20120609')
         .then((resp) => resp.json())
@@ -94,12 +96,22 @@ fetch(venuesSearchUrl)
           let photos = data.response.photos.items
 
           return photos.map(function(photo) {
-            let img = createNode('img')
+            let imgNode = createNode('img')
+                imgSrcValue = photo.prefix + '100x100' + photo.suffix
 
-            img.src = photo.prefix + '100x100' + photo.suffix
+            imgNode.src = (imgSrcValue) ? imgSrcValue : '../img/no-image.jpg'
 
-            append(photoContainer, img)
+            append(photoContainerNode, imgNode)
           })
+        })
+
+      fetch('https://api.foursquare.com/v2/venues/' + idValue + '/?client_id=O4N5MBHQWS11LRWBBO15JTZWFC42WKSUKTQYMXJ1ZN1CIPXD&client_secret=X1043GY3LH0W4S54GT0RWL300R2144W5WUVJKQ30GI0O1F03&v=20120609')
+        .then((resp) => resp.json())
+        .then(function(data) {
+          let ratings = data.response.venue.rating
+          spanNode.innerHTML = (ratings) ? ratings : '-'
+
+          return ratings
         })
     })
   })
